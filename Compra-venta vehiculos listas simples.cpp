d1d152ac6af1f45ddf4d48a44db3b8156641c2ac
VictorManuelLopezHenoa <v.lopez9@utp.edu.co>
2025-03-02 23:05:12 -0500

    Actualizacion

--- Compra-venta vehiculos listas simples.cpp
+++ Compra-venta vehiculos listas simples.cpp
@@ -6,10 +6,19 @@ using namespace std;
 void registraV(struct vehiculos *&raiz);  //*& trabaja con el puntero original
 void caracteristicas(struct vehiculos *&raiz);
 void existencia(struct vehiculos *raiz);
-void listaV(struct vehiculos *raiz);
+void actualizar(struct vehiculos *&raiz);
+void eliminar_activar(struct vehiculos *&raiz);
+void vborrados(struct vehiculos *&raiz, struct eliminados *&raizelim);
+void listaV(struct vehiculos *raiz, struct eliminados *raizelim);
+
+struct eliminados{
+    struct vehiculos *elim;
+    struct eliminados *esig;
+};
 
 struct vehiculos{
-    char placa[7]; char marca[20], modelo[20], color[10], año[5], precio[11], tipo[2], estado[2];
+    char placa[7]; char marca[20], modelo[20], color[10], año[5], tipo[2], estado[2];
+    int precio;
     struct hojadevida *hdv;
     struct vehiculos *vsig;
 };
@@ -19,10 +28,12 @@ struct hojadevida{
     struct hojadevida *hsig;
 };
 
+
+
 int main(){
 
     struct vehiculos *raiz = NULL;
-
+    struct eliminados *raizelim = NULL;
    
     int opc;
     int opc2;
@@ -56,16 +67,16 @@ int main(){
 
         case 3: cout<<"\n\n\tExistencia de vehiculos"<<endl; existencia(raiz); break;
 
-        case 4:
+        case 4: cout<<"\n\n\tActualizar informacion"<<endl; actualizar(raiz); break;
 
-        case 5: 
+        case 5: cout<<"\n\n\tEliminar o activar vehiculos"<<endl; eliminar_activar(raiz); break;
 
-        case 6:
+        case 6: cout<<"\n\n\tMover vehiculos eliminados"<<endl; vborrados(raiz, raizelim); break;
 
-        case 7: cout<<"\n\n\tLista de vehiculos"<<endl; listaV(raiz); break;
+        case 7: cout<<"\n\n\tLista de vehiculos"<<endl; listaV(raiz, raizelim); break;
 
-        case 8:
-
+        case 8: cout<<"\n¡Muchas gracias por visitarnos, vuelve pronto!"<<endl; break;
+        
         default: cout<<"Opcion invalida"<<endl; break;
     }
 
@@ -166,9 +177,9 @@ void caracteristicas(struct vehiculos *&raiz){
 
 void existencia(struct vehiculos *raiz){                                        
     
-    char marcaB[20], modeloB[20], precioMIN[10], precioMAX[10], tipoB[2];
+    char marcaB[20], modeloB[20], tipoB[2];
     int opc;
-
+    int precioMIN, precioMAX;
     struct vehiculos *temp = raiz;
 
     cout<<"\nConsultar existencia por:  "<<endl;
@@ -184,8 +195,8 @@ void existencia(struct vehiculos *raiz){
         cout<<"\nDigite la marca: "; cin>>marcaB;
         cout<<"\n\tVehiculos disponibles"<<endl;
         while(temp != NULL){
-          if(strcmp(temp->marca, marcaB)==0){             //SE IMPRIME DOBLE
-              listaV(temp);
+          if(strcmp(temp->marca, marcaB)==0){            
+              listaV(temp, NULL); // para llamar a listaV con NULL para raizelim
           }
             temp = temp->vsig;
         } break;
@@ -194,33 +205,150 @@ void existencia(struct vehiculos *raiz){
         cout<<"\nDigite el modelo: "; cin>>modeloB;
         cout<<"\n\tVehiculos disponibles"<<endl;
         while(temp != NULL){
-            if(strcmp(temp->modelo, modeloB)==0){         //SE IMPRIME DOBLE
-               listaV(temp);
+            if(strcmp(temp->modelo, modeloB)==0){         //cambiar a listaV, por lo que tiene ella adentro.
+               listaV(temp, NULL);
             }
                temp = temp->vsig;
+        } break;
+
+        case 3: 
+        cout<<"\nDigite el precio minimo: "; cin>>precioMIN;
+        cout<<"Digite el precio maximo: "; cin>>precioMAX;
+        cout<<"\nVehiculos disponible"<<endl;
+        while(temp != NULL){
+         if(precioMIN < temp->precio && precioMAX > temp->precio){
+            listaV(temp, NULL);
+         }
+           temp = temp->vsig;
         } break;
 
         case 4:
         cout<<"\nDigite el tipo: "; cin>>tipoB;
         cout<<"\n\tVehiculos disponibles"<<endl;
-        while(temp != NULL){                                   //FUNCIONA A MEDIAS
+        while(temp != NULL){                                   
             if(strcmp(temp->tipo, tipoB)==0){
-                listaV(temp);
+                listaV(temp, NULL);
             }
              temp = temp->vsig;
         } break;
+ 
+        case 5: cout<<"\nVolviendo al menú...."<<endl; return; break;
 
     default: cout<<"Opcion no valida"<<endl; break;
     }
+}
+
+
+void actualizar(struct vehiculos *&raiz){
     
+    char placaB[7];
+    int opc;
+
+    struct vehiculos *temp = raiz;
+
+    cout<<"\nDigite la placa del vehiculo a actualizar: "; cin>>placaB;
+
+    while(temp != NULL && (strcmp(temp->placa, placaB)!=0)){
+        temp = temp->vsig;
+    }
+
+    if(temp == NULL){
+        cout<<"\nVehiculo con placa "<<placaB<<" no encontrado"<<endl;
+        return;
+    }
+
+    cout<<"\n¿Que informacion desea actualizar?"<<endl;
+    cout<<"1. Precio"<<endl;
+    cout<<"2. Tipo"<<endl;
+    cout<<"\nElija una opcion: "; cin>>opc;
+
+    if(opc == 1){
+        cout<<"\nNuevo precio: "; cin>>temp->precio;
+    } else if (opc == 2){
+        cout<<"\nNuevo tipo (P-propio C-consignado): "; cin>>temp->tipo;
+    } else {
+        cout<<"\nOpcion no valida"<<endl; return;
+    }
+
+    cout<<"\nInformacion actualizada exitosamente"<<endl;
 
 }
 
 
-void listaV(struct vehiculos *raiz){
+void eliminar_activar(struct vehiculos *&raiz){
+
+    char placaB[7]; 
+
+    struct vehiculos *temp = raiz;
+
+    cout<<"\nDigite la placa del vehiculo: "; cin>>placaB;
+
+    while(temp != NULL && strcmp(temp->placa, placaB)!=0){
+        temp = temp->vsig;
+    }
+
+    if(temp == NULL){
+        cout<<"\nVehiculo con placa "<<placaB<<" no encontrado"<<endl;
+        return;
+    }
+
+    if(strcmp(temp->estado, "A")==0){
+        temp->estado[0] = 'E';
+    } else {
+        temp->estado[0] = 'A';
+    }
+
+    cout<<"\nCambio de estado del vehiculo exitoso"<<endl;
+}
+
+
+void vborrados(struct vehiculos *&raiz, struct eliminados *&raizelim){
+
+    struct vehiculos *temp = raiz;
+    struct vehiculos *prev = NULL;
+
+    while(temp != NULL){
+        if(temp->estado[0] == 'E'){
+            struct eliminados *nuevoElim;
+            nuevoElim = new(struct eliminados);
+            nuevoElim->elim = temp;
+            nuevoElim->esig = raizelim;
+            raizelim = nuevoElim;
+
+            if(prev == NULL) {
+                raiz = temp->vsig;
+            } else {
+                prev->vsig = temp->vsig;
+            }
+
+            struct vehiculos *tempElim = temp;
+            temp = temp->vsig;
+            tempElim->vsig = NULL;
+            
+        } else {
+            prev = temp;
+            temp = temp->vsig;
+        }
+    }
+    
+    cout << "\nVehiculos eliminados movidos exitosamente" << endl;
+    return;
+}
+
+
+void listaV(struct vehiculos *raiz, struct eliminados *raizelim){
+
+    int opc;
 
     struct vehiculos *temp = raiz;
    
+
+        cout<<"\nElija la lista a consultar: "<<endl;
+        cout<<"\n1. Vehiculos A-activos"<<endl;
+        cout<<"2. Vehiculos B-borrados"<<endl;
+        cout<<"\nSeleccione una opcion: "; cin>>opc;
+
+        if(opc == 1){
 
     while(temp != NULL){
         
@@ -239,7 +367,31 @@ void listaV(struct vehiculos *raiz){
             temph = temph->hsig;
         }
         temp = temp->vsig;
-    }
-    
+     }
 
-}
+    } else if(opc==2){
+        struct eliminados *tempElim = raizelim;
+        while(tempElim != NULL) {
+            struct vehiculos *temp = tempElim->elim;
+            cout << "\nPlaca: " << temp->placa << endl;
+            cout << "Marca: " << temp->marca << endl;
+            cout << "Modelo: " << temp->modelo << endl;
+            cout << "Color: " << temp->color << endl;
+            cout << "Año: " << temp->año << endl;
+            cout << "Precio: " << temp->precio << endl;
+            cout << "Tipo: " << temp->tipo << endl;
+            cout << "Estado: " << temp->estado << endl;
+
+            struct hojadevida *temph = temp->hdv;
+            while(temph != NULL) {
+                cout << "Caracteristica: " << temph->adicionales << endl;
+                temph = temph->hsig;
+            }
+            tempElim = tempElim->esig;
+        }
+    } else {
+        cout<<"\nOpcion no valida"<<endl;
+        return;
+    }
+    }
+
