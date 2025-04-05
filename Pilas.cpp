19545de2f332c56dcbfc325beb96c0f375272db1
VictorManuelLopezHenoa <v.lopez9@utp.edu.co>
2025-04-04 21:21:03 -0500

    Actualizacion

--- Pilas.cpp
+++ Pilas.cpp
@@ -9,10 +9,10 @@ struct nodo {
 void insertar(nodo *&raiz);
 void imprimir(nodo *&raiz);
 void extraer(nodo *&raiz);
-void cantidad(nodo *&raiz);
+int cantidad(nodo *&raiz);
 void liberar(nodo *&raiz);
-void extraerN(nodo *&raiz);
 void insertarN(nodo *&raiz);
+void extraerN(nodo *&raiz);
 
 int main(){
 
@@ -20,10 +20,10 @@ int main(){
     struct nodo *raiz = NULL;
 
     do{
-    cout<<"\n\tMENÚ PILAS";
+    cout<<"\n\tMENÚ PILAS"<<endl;
     cout<<"\n1. Insertar nodo"<<endl;
     cout<<"2. Imprimir pila"<<endl;
-    cout<<"3. Extraer (Sale último nodo en llegar)"<<endl;
+    cout<<"3. Extraer"<<endl;
     cout<<"4. Cantidad"<<endl;
     cout<<"5. Liberar (Vaciar pila)"<<endl;
     cout<<"6. Salir"<<endl;
@@ -37,25 +37,49 @@ int main(){
         cout<<"\nElija una opcion: "; cin>>opc2;
         if(opc2 == 1){
             insertar(raiz);
-         }else{
+         }else if(opc2 == 2){
             insertarN(raiz);
-         } break;
+         } else {
+            cout<<"\nOpcion no disponible"<<endl;
+         }break;
 
-        case 2: cout<<"\n\tImprimir pila"<<endl; imprimir(raiz); break;
+        case 2: cout<<"\n\tImprimir pila"<<endl;
+        if(raiz == NULL){
+            cout<<"\nLa pila está vacia"<<endl;
+        } else {
+            imprimir(raiz);
+        } break;
 
         case 3: cout<<"\n\tExtraer nodo"<<endl;
+        if(raiz == NULL){
+            cout<<"\nLa pila está vacia"<<endl;
+        } else {
+
         cout<<"\n1. Extraer de la ultima posicion"<<endl;
         cout<<"2. Extraer de la poscision N"<<endl;
         cout<<"\nElija una opcion: "; cin>>opc2;
         if(opc2 == 1){
             extraer(raiz);
-         }else{
+         }else if(opc2 == 2){
             extraerN(raiz);
+         } else {
+            cout<<"\nOpcion no disponible"<<endl;
          } break;
 
-        case 4: cout<<"\n\tCantidad de nodos"<<endl; cantidad(raiz); break; 
+        case 4: cout<<"\n\tCantidad de nodos\n"<<endl; 
+        if(raiz == NULL){
+            cout<<"La pila está vacia"<<endl;
+        } else {
+                cout<<cantidad(raiz)<<endl;
+            }
+        } break; 
 
-        case 5: cout<<"\n\tLiberar pila"<<endl; liberar(raiz); break;
+        case 5: cout<<"\n\tLiberar pila"<<endl; 
+        if (raiz == NULL) {
+            cout<<"La pila ya está vacía."<<endl;
+        } else {
+            liberar(raiz); 
+        } break;
 
         case 6: cout<<"\nHasta luego..."<<endl; break;
 
@@ -81,14 +105,10 @@ void insertar(nodo *&raiz){
 }
 
 void imprimir(nodo *&raiz){
-    if(raiz == NULL){
-        cout<<"\nLa pila está vacia"<<endl;
-        return;
-    }
 
     struct nodo *aux = NULL;
 
-    cout<<"\nLista de nodos: \n\n";
+    cout<<"\nInformacion de nodos: \n\n";
     while(raiz != NULL){
         cout<<raiz->info<<endl;
         struct nodo *temp = raiz;
@@ -107,11 +127,7 @@ void imprimir(nodo *&raiz){
 }
 
 void extraer(nodo *&raiz){
-    if(raiz == NULL){
-        cout<<"\nLa pila está vacia"<<endl;
-        return;
-    }
-
+    
     int informacion = raiz->info;
     struct nodo *elim = raiz;
     raiz = raiz->sig;
@@ -120,19 +136,14 @@ void extraer(nodo *&raiz){
     cout<<"\nDato extraido: "<<informacion<<endl;
 }
 
-void cantidad(nodo *&raiz){
-    if(raiz == NULL){
-        cout<<"\nLa pila está vacia"<<endl;
-        return;
-    }
-
+int cantidad(nodo *&raiz){
     int count = 0;
     struct nodo *aux = NULL;
 
     while(raiz != NULL){
         count++;
         struct nodo *temp = raiz;
-        raiz = raiz->sig;
+        raiz = raiz->sig;                    
         temp->sig = aux;
         aux = temp;
     }
@@ -144,23 +155,14 @@ void cantidad(nodo *&raiz){
         raiz = temp;
     }
 
-    cout<<count<<endl;             //manejar la funcion con int
+ return count;         
 
 }
 
 void liberar(nodo *&raiz){
-    if (raiz == NULL) {
-        cout<<"La pila ya está vacía."<<endl;
-        return;
-    }
-
+   
     struct nodo *reco = raiz;
     struct nodo *bor;
-
-    if (raiz == NULL) {
-        cout<<"La pila ya está vacía."<<endl;
-        return;
-    }
 
     while (reco != NULL) {
         bor = reco;
@@ -171,10 +173,104 @@ void liberar(nodo *&raiz){
     cout<<"Pila liberada." <<endl;
 }
 
-void extraerN(nodo *&raiz){
-
-}
-
 void insertarN(nodo *&raiz){
 
+    int posc;
+    cout<<"\nIngrese la posicion donde desea ingresar el nodo: "; cin>>posc;
+
+    if(posc == (cantidad(raiz) + 1)){
+        insertar(raiz);                         //ultima posicion
+        return;
+    }
+
+    if(posc != 1 && posc > cantidad(raiz) || posc <= 0){
+        cout<<"\nPosicion no disponible"<<endl;
+        return;
+    }
+
+    struct nodo *aux = NULL;
+
+    if(posc == 1){                          //1 posicion
+        if(raiz == NULL){
+            insertar(raiz);
+            return;
+       
+        } else {
+
+            while(raiz != NULL){
+            struct nodo *temp = raiz;
+            raiz = raiz->sig;
+            temp->sig = aux;
+            aux = temp;
+           }
+   
+    }
+      } else {                //posicion x
+
+        while(posc -1 != cantidad(raiz)){
+            struct nodo *temp = raiz;
+            raiz = raiz->sig;
+            temp->sig = aux;
+            aux = temp; 
+        }
+
+    }
+    
+    insertar(raiz);
+
+    while(aux != NULL){
+     struct nodo *temp = aux;
+     aux = aux->sig;
+     temp->sig = raiz;
+     raiz = temp;
+   
 }
+}
+
+void extraerN(nodo *&raiz){
+   
+    int posc;
+    cout<<"\nIngrese la posicion donde desea extraer el nodo: "; cin>>posc;
+
+    if(posc > cantidad(raiz) || posc <= 0){
+        cout<<"\nPosicion no disponible"<<endl;
+        return;
+    }
+
+    if(posc == cantidad(raiz)){      //ultima posicion
+        extraer(raiz);
+        return;
+    }
+
+    struct nodo *aux = NULL;
+
+    if(posc == 1){          //1 posicion
+        
+        while(cantidad(raiz) != 1){
+            struct nodo *temp = raiz;
+            raiz = raiz->sig;
+            temp->sig = aux;
+            aux = temp;
+        }
+
+    } else {         //posicion x
+
+        while(posc != cantidad(raiz)){
+            struct nodo *temp = raiz;
+            raiz = raiz->sig;
+            temp->sig = aux;
+            aux = temp;
+        }
+    
+    }
+        extraer(raiz);
+
+        while(aux != NULL){
+            struct nodo *temp = aux;
+            aux = aux->sig;
+            temp->sig = raiz;
+            raiz = temp;
+        }
+
+    }
+   
