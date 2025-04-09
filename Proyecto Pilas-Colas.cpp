419caf3de9f84a2e7aad139078e712fa316c3a0e
VictorManuelLopezHenoa <v.lopez9@utp.edu.co>
2025-04-09 15:12:18 -0500

    actualizacion

--- Proyecto Pilas-Colas.cpp
+++ Proyecto Pilas-Colas.cpp
@@ -7,13 +7,15 @@ struct datos{
     datos *sig;
 };
 
-void registrar(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo2, datos *&raiz3, datos *&fondo3);
-void elimPersonaCola(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo2, datos *&raiz3, datos *&fondo3);
+void registrar(datos *&raiz, datos *&fondo, int edad);
+void elimPersonaCola(datos *&raiz, datos *&fondo, int edad);
 
 int main(){
 
     int opc;
+    int edad;
 
+    datos *raiz = NULL; datos *fondo = NULL;
     datos *raiz1 = NULL; datos *fondo1 = NULL; 
     datos *raiz2 = NULL; datos *fondo2 = NULL; 
     datos *raiz3 = NULL; datos *fondo3 = NULL; 
@@ -29,11 +31,51 @@ int main(){
 
         switch(opc){
 
-            case 1: cout<<"\n\tRegistro"<<endl; registrar(raiz1, fondo1, raiz2, fondo2, raiz3, fondo3); break;
+            case 1: cout<<"\n\tRegistro"<<endl;
+
+            cout<<"\nIngrese su edad: "; cin>>edad;
+            
+            if(edad > 55){
+                raiz = raiz1;
+                fondo = fondo1;
+
+            } else if(edad <= 55 && edad > 40) {
+                raiz = raiz2;
+                fondo = fondo2;
+
+            } else if(edad <= 40 && edad > 0){
+                raiz = raiz3;
+                fondo = fondo3;
+
+            } else { 
+                cout<<"\nEdad no valida"<<endl;
+            }
+
+            registrar(raiz, fondo, edad); break;
 
             case 2: cout<<"\n\tTurno siguiente"<<endl; break;
 
-            case 3: cout<<"\n\tEliminar persona de la cola"<<endl; elimPersonaCola(raiz1, fondo1, raiz2, fondo2, raiz3, fondo3); break;
+            case 3: cout<<"\n\tEliminar persona de la cola"<<endl; 
+            
+            cout<<"\nIngrese su edad: "; cin>>edad;
+            
+            if(edad > 55){
+                raiz = raiz1;
+                fondo = fondo1;
+
+            } else if(edad <= 55 && edad > 40) {
+                raiz = raiz2;
+                fondo = fondo2;
+
+            } else if(edad <= 40 && edad > 0){
+                raiz = raiz3;
+                fondo = fondo3;
+
+            } else { 
+                cout<<"\nEdad no valida"<<endl;
+            }
+            
+            elimPersonaCola(raiz, fondo, edad); break;
 
             case 4: cout<<"\n\tListado de personas atendidas"<<endl; break;
 
@@ -49,7 +91,7 @@ int main(){
     return 0;
 }
 
-void registrar(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo2, datos *&raiz3, datos *&fondo3){
+void registrar(datos *&raiz, datos *&fondo, int edad){
 
     datos *nuevo = new(struct datos);
 
@@ -59,69 +101,38 @@ void registrar(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo2, dat
     cout<<"Sexo: "; cin.getline(nuevo->sexo, 11);
     cout<<"N° de identificacion: "; cin>>nuevo->id; 
     cout<<"N° de telefono: "; cin>>nuevo->tel;
-    cout<<"Edad: "; cin>>nuevo->edad;
+    nuevo->edad = edad;
     
     nuevo->sig = NULL;
 
-    if(nuevo->edad > 55){
-        if(raiz1 == NULL){
-            raiz1 = nuevo;
-            fondo1 = nuevo;
+        if(raiz == NULL){
+            raiz = nuevo;
+            fondo = nuevo;
         } else {
-            fondo1->sig = nuevo;
-            fondo1 = nuevo;
+            fondo->sig = nuevo;
+            fondo = nuevo;
         }
 
-    } else if (nuevo->edad <= 55 && nuevo->edad > 40) {
-        if(raiz2 == NULL){
-            raiz2 = nuevo;
-            fondo2 = nuevo;
-        } else {
-            fondo2->sig = nuevo;
-            fondo2 = nuevo;
-        }
-
-    } else if (nuevo->edad <= 40 && nuevo->edad > 0){
-        if(raiz3 == NULL){
-            raiz3 = nuevo;
-            fondo3 = nuevo;
-        } else {
-            fondo3->sig = nuevo;
-            fondo3 = nuevo;
-        }
-
-    } else {
-        cout<<"\nEdad no valida"<<endl;
-    }
-   
-    
 }
 
 
-void elimPersonaCola(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo2, datos *&raiz3, datos *&fondo3){
+void elimPersonaCola(datos *&raiz, datos *&fondo, int edad){
+   
+    int idB;
+    cout<<"\nIngrese la identificacion de la persona a eliminar: "<<endl;
 
-    int edadB, idB;
-    cout<<"\nIngrese los siguientes datos de la persona a eliminar: "<<endl;
-    cout<<"\nEdad: "; cin>>edadB;
-    cout<<"Identificacion: "; cin>>idB;
-
-    if(edadB > 55){
-
-    }
-    
     datos *auxR = NULL;
     datos *auxF = NULL;
 
-   
+    int count = 1;
 
     while(raiz != NULL){
-        nodo *temp = raiz;
+        datos *temp = raiz;
         raiz = raiz->sig;
         temp->sig = NULL;
         
-        if(idB == raiz->){
-            nodo *elim = temp;
-            cout<<"\nInformacion del nodo eliminado: "<<elim->info<<endl;
+        if(idB == raiz->id){
+            datos *elim = temp;
             delete elim;
         } else {
             if(auxR == NULL){
@@ -132,9 +143,9 @@ void elimPersonaCola(datos *&raiz1, datos *&fondo1, datos *&raiz2, datos *&fondo
                 auxF = temp;
             }
         }
+        count++;
     }
 
     raiz = auxR;
     fondo = auxF;
-
 }
